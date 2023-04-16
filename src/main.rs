2e4564095412bac12f83923c0803b49b1147b47b
256k <fayek.helmi@gmail.com>
2023-04-16 11:12:56 -0400

    finalized basic functionality of addition and listing. needd to store data somewhere

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env::{self, Args};
+use std::{env::{self}};
 
 // entry object
 // each entry will have a string and a date
@@ -13,9 +13,16 @@ struct Entry {
     date: i64,
     y: String,
 }
+impl Entry {
+    fn days_elapsed(&self) -> i64 {
+        return (Utc::now().timestamp() - self.date) / 60 / 60 / 24;
+    }
+}
 
-fn days_since(start_date: i64, end_date: i64) -> i64 {
-    return (end_date - start_date) / 60 / 60 / 24;
+fn list_entries(entries: &Vec<Entry>) {
+    for entry in entries.iter() {
+        println!("{} days since {}", entry.days_elapsed(), entry.y);
+    }
 }
 
 fn add_entry(entry_text: &str) -> Entry {
@@ -26,57 +33,35 @@ fn add_entry(entry_text: &str) -> Entry {
 }
 
 fn main() {
-    // interactive_mode();
     let mut entries: Vec<Entry> = Vec::new();
     let cli_args: Vec<String> = env::args().collect();
-    for _arg in cli_args.iter() {
-        // println!("{}", arg);
-    }
     if cli_args.len() > 1 {
         match cli_args[1].as_str() {
             "add" => {
-                println!("you want me to add what, Boss?");
-                entries.push(add_entry(&cli_args[2]));
-            }
-            "run" => {
                 interactive_mode(&mut entries);
             }
-            _ => println!("no add no foul"),
+            _ => list_entries(&entries),
         };
     }
-    for entry in entries.iter() {
-        println!("{}", entry.y);
-        println!("{}", entry.date / 60 / 60 / 24);
-    }
-    // let now = Utc::now().timestamp();
-    // const TESTTIME: i64 = 1680411536;
-    // let days = days_since(TESTTIME, now);
-    // println!("days: {}", days );
-    // let my_entry = add_entry("hello");
-    // println!("{}", my_entry.y);
 }
 
 fn interactive_mode(entries: &mut Vec<Entry>) {
-    println!("welcome to XdsY");
     loop {
-        println!(">");
+        println!("add new entry:");
         let mut user_input = String::new();
         std::io::stdin().read_line(&mut user_input).unwrap();
-        println!(" nothing? {}", user_input);
-        match user_input.as_str().trim_end() {
-            // TODO: convert user_input from a single string into an array 
-            // of strings separated by spaces to be able to pass the 
-            // second argument as the name of the entry in the add_entry fn
-            "add" => entries.push(add_entry(&user_input)),
-            "list" => list_entries(entries),
-            "exit" => break,
-            _ => println!("enter valid command"), 
-        }
+        let user_input_array: Vec<&str> = user_input.split(" ").collect();
+            match user_input_array[0].trim_end() {
+                    "/list" => list_entries(entries),
+                    "/exit" => break,
+                    _ =>{ 
+                    if user_input_array[0].len() > 1 {
+                        entries.push(add_entry(&user_input));
+                    }else {
+                        println!("please enter a valid entry")
+                    }}
+                } 
+            }
     }
-}
 
-fn list_entries(entries: &Vec<Entry>) {
-    for entry in entries.iter() {
-        println!("{} days since {}", entry.date, entry.y);
-    }
-}
+
